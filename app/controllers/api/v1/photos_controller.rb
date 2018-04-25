da3dd8978c22ce45876af067390db424628b7e78
Yipeechen <yipeechen0418@gmail.com>
2018-04-25 11:33:19 +0800

    add show action for photo api

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -11,4 +11,21 @@ class Api::V1::PhotosController < ApiController
       end
     }
   end
+
+  def show
+    @photo = Photo.find_by(id: params[:id])
+    # 若無此id照片，回傳HTTP狀態碼400
+    if !@photo  
+      render json: {
+        message: "Can't find the photo!",
+        status: 400
+      }
+    else
+      render json: {
+        title: @photo.title,
+        date: @photo.date,
+        description: @photo.description
+      }
+    end
+  end
 end
