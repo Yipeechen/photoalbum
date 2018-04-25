e9d47191649af24d86e522ed25605374f163684d
Yipeechen <yipeechen0418@gmail.com>
2018-04-25 11:37:57 +0800

    add notes

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::PhotosController < ApiController
   def index
     @photos = Photo.all
+    #當有多筆資料時習慣在資料外包覆一層json: { data: @varrible }
+    #使用map方法使映射出另一個新的array
     render json: {
       data: @photos.map do |photo|
         {
@@ -13,6 +15,7 @@ class Api::V1::PhotosController < ApiController
   end
 
   def show
+    #使用find_by方法，拋出物件內容為nil，避免拋出error
     @photo = Photo.find_by(id: params[:id])
     # 若無此id照片，回傳HTTP狀態碼400
     if !@photo  
